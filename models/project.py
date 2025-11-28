9b24e276138334ac46e3f5dc2acddc442ae15d83
Tim Kindt <Tim.kindt@hotmail.com>
2025-11-28 17:42:58 +0100

    Rename 'id' to 'proj_id' in Project class for clarity

--- models/project.py
+++ models/project.py
@@ -1,11 +1,11 @@
 # models\project.py
 
 class Project:
-    def __init__(self, name:str, description:str, id:int=None, archived:bool=False ):
+    def __init__(self, name:str, description:str, proj_id:int=None, archived:bool=False ):
         self.name = name
         self.description = description
         self.work_sessions = []
-        self.id = id
+        self.proj_id = proj_id
         self.archived = archived
 
     def archive(self):
@@ -18,10 +18,10 @@ class Project:
         self.work_sessions.append(work_session)
 
     def __repr__(self):
-        return f"Project(id={self.id}, name={self.name}, description={self.description}, archived={self.archived})"
+        return f"Project(id={self.proj_id}, name={self.name}, description={self.description}, archived={self.archived})"
 
     def __str__(self):
-        return f"Project: {self.name} (ID: {self.id})\nDescription: {self.description}\nArchived: {self.archived}\nWork Sessions: {len(self.work_sessions)}"
+        return f"Project: {self.name} (ID: {self.proj_id})\nDescription: {self.description}\nArchived: {self.archived}\nWork Sessions: {len(self.work_sessions)}"
 
 
 
