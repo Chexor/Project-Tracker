491d9b5f76cffe3833833dfe9dbc54d0904deffc
Tim K. <Tim.kindt@hotmail.com>
2025-12-01 09:57:41 +0100

    Initialize database and start main menu in main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from data.database import Database
 from ui.main_menu import MainMenu
 
 if __name__ == "__main__":
+
+    # Initialiseer de database
     db = Database()
+
+    # Start het hoofdmenu
     menu = MainMenu(db)
     menu.run()
